fda326ff32eed6ff59858cc71ebfdf7e0610a18a
daniel.kocsan <dani@kocsan.hu>
2013-11-23 11:53:37 +0100

    grunt deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,8 @@ module.exports = function(grunt) {
     });
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-deploy");
     
     grunt.registerTask('default', ['less']);
-}
+};
+/*0ff1c3f00D*/
